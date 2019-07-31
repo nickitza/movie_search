742b0d329ce21a8f2f642db2fc7290f776ac7ebf
Nicki Nixon <nickitza@gmail.com>
2019-07-31 10:58:57 -0600

    search.ejs for root route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var request = require("request")
 var app = express()
 app.set("view engine", "ejs")
 
+app.get('/', function(req, res){
+  res.render('search')
+})
+
 app.get('/results', function(req, res){
   request("http://www.omdbapi.com/?apikey=thewdb&s=utah", function(err, response, body){
     if(!err && response.statusCode === 200){
